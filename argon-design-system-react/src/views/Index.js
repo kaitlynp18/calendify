59ca35c5e01b858e2c5852709827b3971087f516
kaitlyn <kaitlynp18@yahoo.ca>
2024-01-27 17:25:08 -0500

    create frontend

---
+++ argon-design-system-react/src/views/Index.js
@@ -0,0 +1,50 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.1.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+//import { Container, Row } from "reactstrap";
+
+// core components
+import DemoNavbar from "components/Navbars/DemoNavbar.js";
+import SimpleFooter from "components/Footers/SimpleFooter.js";
+
+// index page sections
+import Hero from "./IndexSections/Hero.js";
+
+
+class Index extends React.Component {
+  componentDidMount() {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    this.refs.main.scrollTop = 0;
+  }
+  render() {
+    return (
+      <>
+        <DemoNavbar />
+        <main ref="main">
+          <Hero />
+        </main>
+        <SimpleFooter />
+      </>
+    );
+  }
+}
+
+export default Index;
---
+++ argon-design-system-react/src/views/IndexSections/Hero.js
@@ -0,0 +1,160 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.1.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React, { useState } from 'react';
+import { useDropzone } from "react-dropzone";
+
+// reactstrap components
+import { Button, Container, Row, Col } from "reactstrap";
+
+const thumbsContainer = {
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  marginTop: 16
+};
+
+const thumb = {
+  display: 'inline-flex',
+  borderRadius: 2,
+  border: '1px solid #eaeaea',
+  marginBottom: 8,
+  marginRight: 8,
+  width: 100,
+  height: 100,
+  padding: 4,
+  boxSizing: 'border-box'
+};
+
+const thumbInner = {
+  display: 'flex',
+  minWidth: 0,
+  overflow: 'hidden'
+};
+
+const img = {
+  display: 'block',
+  width: 'auto',
+  height: '100%'
+};
+
+const UploadComponent = () => {
+  const {getRootProps, getInputProps, open, acceptedFiles} = useDropzone({
+    // Disable click and keydown behavior
+    noClick: true,
+    noKeyboard: true
+  });
+
+  const files = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+    return (
+      <>
+        <div className="position-relative">
+          {/* Hero for FREE version */}
+          <section className="section section-hero section-shaped" style={{ height: "100vh" }}>
+            {/* Background circles */}
+            <div className="shape shape-style-1 shape-default">
+              <span className="span-150" />
+              <span className="span-50" />
+              <span className="span-50" />
+              <span className="span-75" />
+              <span className="span-100" />
+              <span className="span-75" />
+              <span className="span-50" />
+              <span className="span-100" />
+              <span className="span-50" />
+              <span className="span-100" />
+            </div>
+            <Container className="shape-container d-flex align-items-center py-lg">
+              <div className="col px-0">
+                <Row className="align-items-center justify-content-center">
+                  <Col className="text-center" lg="6">
+                    <img
+                      alt="..."
+                      className="img-fluid"
+                      src={require("assets/img/brand/argon-react-white.png")}
+                      style={{ width: "200px" }}
+                    />
+                    <p className="lead text-white">
+                      Calendify - a web app to automatically generate a calendar from your syllabus.
+                    </p>
+                    <div className="btn-wrapper mt-5">
+                      <Button
+                        className="btn-white btn-icon mb-3 mb-sm-0"
+                        color="default"
+                        href="https://www.creative-tim.com/product/argon-design-system-react?ref=adsr-landing-page"
+                        size="lg"
+                      >
+                        <span className="btn-inner--icon mr-1">
+                          <i className="ni ni-cloud-download-95" />
+                        </span>
+                        <span className="btn-inner--text">Download React</span>
+                      </Button>{" "}
+                      <Button
+                        className="btn-icon mb-3 mb-sm-0"
+                        color="github"
+                        href="https://github.com/creativetimofficial/argon-design-system-react"
+                        size="lg"
+                        target="_blank"
+                      >
+                        <span className="btn-inner--icon mr-1">
+                          <i className="fa fa-github" />
+                        </span>
+                        <span className="btn-inner--text">
+                          <span className="text-warning mr-1">Star us</span>
+                          on Github
+                        </span>
+                      </Button>
+                    </div>
+                    <div className="mt-5">
+                      <small className="text-white font-weight-bold mb-0 mr-2">
+                        *proudly coded by
+                      </small>
+                      <img
+                        alt="..."
+                        className="ml-1"
+                        style={{ height: "28px" }}
+                        src={require("assets/img/brand/creativetim-white-slim.png")}
+                      />
+                    </div>
+                    <div className="upload-container" style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}>
+                    <div {...getRootProps({className: 'dropzone'})}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here</p>
+        <button type="button" onClick={open}>
+          Open File Dialog
+        </button>
+      </div>
+      <aside>
+        <h4>Files</h4>
+        <ul>{files}</ul>
+      </aside>
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+            </Container>
+          </section>
+        </div>
+      </>
+    );
+  }
+
+export default UploadComponent;
